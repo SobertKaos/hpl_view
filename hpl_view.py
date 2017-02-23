1109eff86c88a303844e77dac498f20d4e09e831
SobertKaos <Karkkainen.a@gmail.com>
2017-02-23 20:11:08 +0100

    Initial commit with basic framework

---
+++ hpl_view.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""hpl_view.py: Collects data for specified bus/tram stops
+from Vasttrafik and presents departures """
+
+__author__ = "Alexander Kärkkäinen"
+
+
+class Model(object):
+    """docstring for Model"""
+    def __init__(self, arg):
+        super(Model, self).__init__()
+        self.arg = arg
+
+
+class Viewer(object):
+    """docstring for Viewer"""
+    def __init__(self, arg):
+        super(Viewer, self).__init__()
+        self.arg = arg
+
+
+class Controller(object):
+    """docstring for Controller"""
+    def __init__(self, arg):
+        super(Controller, self).__init__()
+        self.arg = arg
+
+if __name__ == '__main__':
+    pass
